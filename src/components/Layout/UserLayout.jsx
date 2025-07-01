9da481cda9f1b8690cd5a1626430bdd03b592bcc
S <sarimansari313@gmail.com>
2025-07-02 00:40:16 +0530

    footer

--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 
 const UserLayout = () => {
   return (
     <>
       <Header />
       {/*Main Content*/}
-      {/*Footer*/}
+      <Footer />
     </>
   );
 };
